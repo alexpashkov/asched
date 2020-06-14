45f179e561bd3743c26e1b30f725f15ae0c96883
Alex Pashkov <alexpashkov123@gmail.com>
2020-06-14 16:09:38 +0300

    Log config value

--- internal/amenities/amenities_test.go
+++ internal/amenities/amenities_test.go
@@ -14,10 +14,11 @@ func TestAmenitiesService(t *testing.T) {
 	if testing.Short() {
 		t.Skip("short testing enabled")
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-	t.Cleanup(cancel)
 	conf, err := config.ReadConfig(t.Logf)
 	require.NoError(t, err)
+	t.Logf("config is %#v", conf)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	t.Cleanup(cancel)
 	client, err := mongo.Connect(ctx)
 	require.NoError(t, err)
 	t.Cleanup(func() {
